92c6531d8983f0d35caa1045c762056fbf1c2f4d
cowanweks <cowannwekesa@gmial.com>
2024-06-19 17:47:38 +0300

    Updated

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,9 +5,9 @@ import { twMerge } from "tailwind-merge";
 
 let API_URL: string;
 
-API_URL = 'http://127.0.0.1:3000/ringstech/api/v1'
+// API_URL = 'http://127.0.0.1:3000/ringstech/api/v1'
 
-// API_URL = 'https://samsungrepair-backend-35d7356462b8.herokuapp.com/ringstech/api/v1/'
+API_URL = 'https://samsungrepair-backend-35d7356462b8.herokuapp.com/ringstech/api/v1/'
 
 
 function cn(...inputs: ClassValue[]) {
