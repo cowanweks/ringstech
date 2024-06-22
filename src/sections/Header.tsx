0e612a5722b807b2708ff620c0be47f55825dde8
cowanweks <cowannwekesa@gmial.com>
2024-06-22 15:32:07 +0300

    Updated Frontend

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -76,20 +76,26 @@ const NavBar = () => {
           </button>
         </div>
       </div>
-      <div className={`md:hidden ${isOpen ? 'block' : 'hidden'}`}>
+      <div className={`md:hidden ${isOpen ? 'block' : 'hidden'} bg-gray-100`}>
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          <a href="shop" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-100">
+          <a href="/shop" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-200">
+            SHOP
+          </a>
+          <a href="/shop?category=phone" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-200">
             SMARTPHONES
           </a>
-          <a href="shop" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-100">
+          <a href="/shop?category=accessory" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-200">
             ACCESSORIES
           </a>
-          <a href="/shop" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-100">
+          <a href="shop" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-200">
+            LIFESTYLE
+          </a>
+          <a href="/shop?category=covers_protectors" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-200">
             COVERS
           </a>
           <HoverCard>
             <HoverCardTrigger asChild>
-              <button className="block px-3 py-2 w-full text-left rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-100">
+              <button className="block px-3 py-2 w-full text-left rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-200">
                 REPAIRS
               </button>
             </HoverCardTrigger>
@@ -105,10 +111,14 @@ const NavBar = () => {
               <div className="h-8">8: Speaker or Microphone Repair</div>
             </HoverCardContent>
           </HoverCard>
-          <a href="/track" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-100">
-            TRACK
+          <a href="/track" className="flex gap-x-4 px-3 py-2 rounded-md text-base font-medium text-gray-600  hover:text-[#ff7701] hover:bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+            </svg>
+            <span>TRACK</span>
           </a>
-          <a href="/cart" className="flex gap-x-4 px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-100">
+          <a href="/cart" className="flex gap-x-4 px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-200">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-gray-600 leading-10 hover:text-[#ff7701] transition ease-in-out  hover:scale-105">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
             </svg>
@@ -116,7 +126,7 @@ const NavBar = () => {
           </a>
           <HoverCard>
             <HoverCardTrigger asChild>
-              <button className="flex gap-x-4 w-full px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-100">
+              <button className="flex gap-x-4 w-full px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-[#ff7701] hover:bg-gray-200">
                 <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/mpesa.png" alt="mpesa" />
                 <span>PayBill</span>
               </button>
---
+++ src/sections/Repairs.tsx
@@ -0,0 +1,120 @@
+import { useState } from "react"
+import { HiMenu as MenuIcon, HiX as CloseIcon } from "react-icons/hi";
+import { Button } from "@shadcn-ui/button"
+import { FaWhatsapp as WhatsappIcon } from "react-icons/fa6"
+
+import Image1 from "@public/repairs/button-fixing.jpg"
+import Image2 from "@public/repairs/camera-repair.jpg"
+import Image3 from "@public/repairs/mother-board-repair.jpg"
+import Image4 from "@public/repairs/software-troubleshooting.jpg"
+import Image5 from "@public/repairs/speaaker and Microphone Repair.jpg"
+
+
+const items = [
+	{
+		image: Image1,
+		service: "Button Repair & Fixing",
+		description: "Fixing mulfunctioning home buttons, or power buttons."
+	},
+	{
+		image: Image2,
+		service: "Camera Repair",
+		description: "Repairing or replacing of faulty Cameras"
+	},
+	{
+		image: Image3,
+		service: "Motherboard Repair",
+		description: "Addressing compleax internal issues involving the phone's motherboard"
+	},
+	{
+		image: Image4,
+		service: "Software Troubleshooting",
+		description: ""
+	},
+	{
+		image: Image5,
+		service: "",
+		description: "Speaker & Microphone Repair"
+	}
+]
+
+function ItemsCard(item: { image: string | undefined, service: string | undefined, description: string | undefined }) {
+
+	return <div className="bg-gray-200">
+		<div className="h-[56.25vw]">
+			<img src={item.image} alt="" className="" />
+		</div>
+		<div className="">
+			<p className="description py-4">
+				{item.service}
+			</p>
+			<p className="description py-4">
+				{item.description}
+			</p>
+			<div className="py-2 flex gap-x-4 justify-center">
+				<a href="">
+					<Button>
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+						</svg>
+						<span>Call</span>
+					</Button>
+				</a>
+				<a href="">
+					<Button>
+						<WhatsappIcon />
+						<span>Chat Us</span>
+					</Button>
+				</a>
+			</div>
+		</div>
+	</div>
+}
+
+
+export default function LifeStyle() {
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
+
+	return (
+		<section id="Repairs" className="min-h-dvh px-4 py-16">
+			<div className="bg-white shadow-md">
+				<div className="container mx-auto mb-16 border px-6 py-3 flex justify-between items-center">
+					<h1 className="font-bold text-lg">REPAIRS</h1>
+					<div className="md:hidden z-10">
+						<button onClick={toggleMenu} className="text-gray-800 focus:outline-none">
+							{isOpen ? <CloseIcon size={24} /> : <MenuIcon size={24} />}
+						</button>
+					</div>
+					<ul className={`flex-col md:flex md:flex-row font-medium gap-x-6 absolute md:static w-full md:w-auto bg-white md:bg-transparent ${isOpen ? 'flex' : 'hidden'} md:flex`}>
+						<a onClick={toggleMenu} href="" className="text-[#ff7701] cursor-pointer hover:text-[#ff7701] my-0 leading-10">
+							<li className="py-2 md:py-0 px-6 md:px-0">ALL</li>
+						</a>
+						<a onClick={toggleMenu} href="" className="my-0 leading-10 cursor-pointer hover:text-[#ff7701]">
+							<li className="py-2 md:py-0 px-6 md:px-0">GEARS & DEVICES</li>
+						</a>
+						<a onClick={toggleMenu} href="" className="my-0 leading-10 cursor-pointer hover:text-[#ff7701]">
+							<li className="py-2 md:py-0 px-6 md:px-0">AUDIO</li>
+						</a>
+						<a onClick={toggleMenu} href="" className="my-0 leading-10 cursor-pointer hover:text-[#ff7701]">
+							<li className="py-2 md:py-0 px-6 md:px-0">CAMERA & VISUAL</li>
+						</a>
+						<a onClick={toggleMenu} href="" className="my-0 leading-10 cursor-pointer hover:text-[#ff7701]">
+							<li className="py-2 md:py-0 px-6 md:px-0">POWERBANK & CHARGING</li>
+						</a>
+					</ul>
+				</div>
+			</div>
+
+			<div className="accessories grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-6">
+				{items.length > 0 ? items.map((item, index) => (<ItemsCard service={item.service} image={item.image} description={item.description} key={index} />)) : ""}
+
+			</div>
+		</section>
+	)
+}
